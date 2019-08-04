2e9269bbe7ce86a528f9123c8fb9d296ec6a3e73
Vetrual Poole <vetrualpoole@gmail.com>
2019-08-04 18:55:25 +0000

    lesson 22 and 23

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,9 +18,20 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id]) 
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+  
+  def update
+      @place = Place.find(params[:id])
+      @place.update_attributes(place_params)
+      redirect_to root_path
+  end
+
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
+
 end
