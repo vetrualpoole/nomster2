ab12510938ffe34851e530a9ab4cdf30fe1f4abe
Vetrual Poole <vetrualpoole@gmail.com>
2019-09-12 23:56:49 +0000

    map not showing

--- db/migrate/20190814013209_alter_places_add_lat_and_lng.rb
+++ db/migrate/20190814013209_alter_places_add_lat_and_lng.rb
@@ -1,4 +1,4 @@
-class AlterPlacesAddLatAndLng < ActiveRecord::Migration[5.2]
+class AlterPlacesAddLatAndLng < ActiveRecord::Migration[5.0]
   def change
     add_column :places, :latitude, :float
     add_column :places, :longitude, :float
